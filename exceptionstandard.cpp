6502e7de9ea084782c85a83ea0cb261981f4651d
Pannntastic <pannntastic@gmail.com>
2024-06-04 18:19:49 +0700

    memanggil elemen ke 5 array

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -1,1 +1,19 @@
 #include <exception>
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main (){
+    cout << "Awal Program" << endl;
+    try
+    {
+        array<int, 3> data = {6, 7, 8};
+        cout << data.at(5) << endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+}
