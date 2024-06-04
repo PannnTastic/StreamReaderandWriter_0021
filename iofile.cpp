9e999ca5ded90e70e06e5350724553417808d4f6
Pannntastic <pannntastic@gmail.com>
2024-06-04 17:32:19 +0700

    prettier

--- iofile.cpp
+++ iofile.cpp
@@ -3,19 +3,22 @@
 #include <fstream>
 using namespace std;
 
-int main (){
+int main()
+{
     string baris;
 
     ofstream outfile;
 
     outfile.open("contoh.txt");
 
-    cout <<">= Menulis File, \'q\' untuk keluar" << endl;
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
 
-    while (true){
+    while (true)
+    {
         cout << "- ";
-        getline (cin , baris);
-        if(baris == "q") break;
+        getline(cin, baris);
+        if (baris == "q")
+            break;
         outfile << baris << endl;
     }
     outfile.close();
@@ -23,12 +26,16 @@ int main (){
     ifstream infile;
     infile.open("contoh.txt");
 
-    cout << endl << ">= Membuka dan membaca file " << endl;
-    if(infile.is_open()){
-        while(getline(infile, baris)){
+    cout << endl
+         << ">= Membuka dan membaca file " << endl;
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
             cout << baris << "\n";
         }
         infile.close();
     }
-    else cout << "Unable to open file";
+    else
+        cout << "Unable to open file";
 }
