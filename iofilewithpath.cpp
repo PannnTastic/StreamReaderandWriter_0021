2a1191d601d63afb9ea0f48ce6bac3168babb308
Pannntastic <pannntastic@gmail.com>
2024-06-04 17:42:02 +0700

    menyalin dri program pertama

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -12,4 +12,31 @@ int main(){
 
     ofstream outfile;
     outfile.open(namafile + ".txt" , ios::out);
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+
+    while (true)
+    {
+        cout << "- ";
+        getline(cin, baris);
+        if (baris == "q")
+            break;
+        outfile << baris << endl;
+    }
+    outfile.close();
+
+    ifstream infile;
+    infile.open( namafile + ".txt");
+
+    cout << endl
+         << ">= Membuka dan membaca file " << endl;
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << "\n";
+        }
+        infile.close();
+    }
+    else
+        cout << "Unable to open file";
 }
