0d3f434c704ce3599c1817331282f6cc5f26d866
Pannntastic <pannntastic@gmail.com>
2024-06-04 18:37:36 +0700

    menambahkan komen

--- manualexception.cpp
+++ manualexception.cpp
@@ -5,13 +5,16 @@ int main (){
     try
     {
         cout << "Selamat Belajar di Prodi TI UMY\n";
+        // throw 0.5; melemparkan sebuah int maka
         cout << "Pernyataan tidak akan d eksekusi\n";
     }
     catch(int a)
     {
+        // blok ini akan d eksekusi
         cout << "Pengecualian akan d eksekusi\n";
     }
     catch(...){
+        // jika selain int maka blok ini akan d eksekusi
         cout << "Default pengecualian d eksekusi\n";
     }
     
